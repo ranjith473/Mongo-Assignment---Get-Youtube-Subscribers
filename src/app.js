e4edfcf25e5eb2779d1f6e226ffbd6d0cce0fe5e
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-02-15 22:43:15 +0530

    chaned

--- src/app.js
+++ src/app.js
@@ -1,42 +1,28 @@
 const express = require("express");
+const Subscriber = require("./models/subscribers");
 const app = express();
-const mongoose = require("mongoose");
-const subscriberModel = require("./models/subscribers");
-const data = require("./data");
 
 // Your code goes here
-const db = mongoose.connection;
-
 app.get("/subscribers", async (req, res) => {
-  const subscribers = await subscriberModel.find({});
-  res.send(subscribers);
+  const result = await Subscriber.find();
+  res.send(result);
 });
 
 app.get("/subscribers/names", async (req, res) => {
-  const subscribers = await subscriberModel.find({});
-
-  const names = subscribers.map((subscriber) => {
-    return {
-      name: subscriber.name,
-      subscribedChannel: subscriber.subscribedChannel
-    };
+  const result = await Subscriber.find().select({
+    name: 1,
+    subscribedChannel: 1,
+    _id: 0
   });
-
-  res.send(names);
+  res.send(result);
 });
 
 app.get("/subscribers/:id", async (req, res) => {
-  const id = req.params.id;
-
   try {
-    const subscriber = await subscriberModel.findOne({ _id: id });
-    if (subscriber == null) {
-      res.status(400).send({ message: "Invalid id" });
-    } else {
-      res.send(subscriber);
-    }
-  } catch (e) {
-    res.status(400).send({ message: e.message });
+    const result = await Subscriber.findById(req.params.id);
+    res.send(result);
+  } catch (err) {
+    return res.status(400).send({ message: err.message });
   }
 });
 
