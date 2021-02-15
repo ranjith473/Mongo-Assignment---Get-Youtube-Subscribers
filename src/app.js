60ea984a9cb801f197bf2bba37f44d3d3733aa7c
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-02-15 21:40:11 +0530

    code

--- src/app.js
+++ src/app.js
@@ -1,28 +1,44 @@
-
-const express = require('express');
-const app = express()
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const subscriberModel = require("./models/subscribers");
+const data = require("./data");
 
 // Your code goes here
+const db = mongoose.connection;
 
+app.get("/subscribers", async (req, res) => {
+  const subscribers = await subscriberModel.find({});
+  res.send(subscribers);
+});
 
+app.get("/subscribers/names", async (req, res) => {
+  const subscribers = await subscriberModel.find({});
 
+  const names = subscribers.map((subscriber) => {
+    return {
+      name: subscriber.name,
+      subscribedChannel: subscriber.subscribedChannel
+    };
+  });
 
+  res.send(names);
+});
 
+app.get("/subscribers/:id", async (req, res) => {
+  const id = req.params.id;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  try {
+    const subscriber = await subscriberModel.findOne({ _id: id });
+    if (subscriber == null) {
+      res.status(400).send({ message: "Invalid id" });
+    } else {
+      res.send(subscriber);
+    }
+  } catch (e) {
+    res.status(400).send({ message: e.message });
+  }
+});
+module.exports = app;
 
 module.exports = app;
